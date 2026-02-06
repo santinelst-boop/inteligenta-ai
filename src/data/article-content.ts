6f3a011c17a151a98e88c7ab1d2be5a94ffbe217
santinelst-boop <santinel.st@gmail.com>
2026-02-06 13:34:44 -0800

    feat: add articles-part3 import to article-content.ts

--- src/data/article-content.ts
+++ src/data/article-content.ts
@@ -1,7 +1,9 @@
 import { articlesPart1 } from "./articles-part1";
 import { articlesPart2 } from "./articles-part2";
+import { articlesPart3 } from "./articles-part3";
 
 export const articleContent: Record<string, { title: string; metaDescription: string; content: string }> = {
   ...articlesPart1,
   ...articlesPart2,
+  ...articlesPart3,
 };
