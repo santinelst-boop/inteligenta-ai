0189adabcb9046979fff341fc7ca0bbc0e34bdd8
santinelst-boop <santinel.st@gmail.com>
2026-02-08 19:28:47 +0200

    feat: import articles-part4 in article-content aggregator

--- src/data/article-content.ts
+++ src/data/article-content.ts
@@ -1,9 +1,11 @@
 import { articlesPart1 } from "./articles-part1";
 import { articlesPart2 } from "./articles-part2";
 import { articlesPart3 } from "./articles-part3";
+import { articlesPart4 } from "./articles-part4";
 
 export const articleContent: Record<string, { title: string; metaDescription: string; content: string }> = {
   ...articlesPart1,
   ...articlesPart2,
   ...articlesPart3,
+  ...articlesPart4,
 };
